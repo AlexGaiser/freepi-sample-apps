5e5c7900f01c0c821a697473377b7e5f269135c9
AlexGaiser <alexcgaiser@gmail.com>
2021-03-15 20:46:15 -0400

    updates node sample with new version freepi

--- sample-node/app.js
+++ sample-node/app.js
@@ -1,13 +1,15 @@
 // const { JSONPlaceholder } = require("@freepi/JSONPlaceholder");
+const dotenv = require("dotenv");
+dotenv.config();
 const { JSONPlaceholder } = require("freepi");
 const jsonWrapper = new JSONPlaceholder();
-
+const { Unsplash } = require("@freepi/unsplash");
 jsonWrapper.todos.find({ id: 1 }).then((data) => {
   console.log(data);
 });
-
+let unsplash = new Unsplash({ access_key: process.env.UNSPLASH_CLIENT_ID });
 const { RequestBuilder } = require("@freepi/core");
-
+unsplash.photos.getAll().then((data) => console.log("unsplash data", data));
 const req = new RequestBuilder({
   url: "https://jsonplaceholder.typicode.com",
 });
